45a56cb7085f72d69e0d6726d071318d503324bf
WinandVM <winand_vanmeerbeek@hotmail.com>
2023-09-14 13:46:09 +0200

    Image & footeraanpassing + EN

--- src/components/AppTrajectEnODIS.vue
+++ src/components/AppTrajectEnODIS.vue
@@ -3,8 +3,8 @@
     <div class="px-4 py-6 my-5 text-center">
     <h1 class="display-5 my-5 fw-bold text-body-emphasis">ODIS Renewal project 2022-2027</h1>
     <div class="col-lg-7 mx-auto">
-      <p class="lead mb-4">The Research Foundation - Flanders (FWO) awarded a grant in 2022 for the ‘KADOC-ODIS:  'Digital Research Infrastructure for Religion, Culture and Society' project. This investment programme, to be implemented over the period 2022-2027, aims to respond to the needs of the various ODIS partners and ensure that the functionalities of ODIS continue to meet the requirements of current and future research in the field of <i>digital humanities</i>.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <p class="lead mb-4">The Research Foundation - Flanders (FWO) awarded a grant in 2022 for the 'KADOC-ODIS: Digital Research Infrastructure for Religion, Culture and Society' project, a core facility of KU Leuven. This investment programme, to be implemented over the period 2022-2027, aims to respond to the needs of the various ODIS partners and ensure that the functionalities of ODIS continue to meet the requirements of current and future research in the field of digital humanities.</p>
+      <div class="d-grid pt-2 gap-2 d-sm-flex justify-content-sm-center">
         <a href="https://www.odis.be/hercules/_en_home.php" target="_blank" class="btn btn-primary btn-lg px-4 gap-3" role="button" aria-disabled="true">ODIS</a>
         <a href="https://kadoc.kuleuven.be/english/3_research/31_ourresearch/ODIS" target="_blank" class="btn btn-outline-secondary btn-lg" role="button" aria-disabled="true">KADOC-ODIS</a>      
       </div>
@@ -12,17 +12,17 @@
     <div class="shadow-lg p-3 mb-2 my-5 background-bar rounded"></div>
   </div>
 
-  <div class="px-4 py-2 text-center">
+  <div class="px-4 py-2 mb-5 text-center">
     <h1 class="display-6 my-4 fw-bold text-body-emphasis">Technical update</h1>
     <div class="col-lg-7 mx-auto">
-      <p class="lead mb-4">The programme envisages a <b>technical renewal</b> of ODIS, including a thorough update of the database interface and software. <b>New tools</b> are introduced <b>to process and analyse data</b>, including interactive visualisation tools that allow users to explore datasets graphically and spatially and display the results of network and prosopographical searches. Furthermore, a <b>new layer for interoperability, interconnectivity and discoverability</b> will be added, facilitating sustainable connections with other (international) research tools. This allows researchers to export and process datasets into their own software and increases the visibility of ODIS in search results of popular search engines. Finally, there will be a <b>new module for collecting and contextualising historical statistical source material</b>. Researchers will be able to download and reuse numerical series and upload their own statistical datasets. 
+      <p class="lead mb-4">The programme envisages a <b>technical renewal</b> of ODIS, including a thorough update of the database interface and software. <b>New tools</b> are introduced <b>to process and analyse data</b>, including interactive visualisation tools that enable users to explore datasets graphically and spatially and display the results of network and prosopographical searches. Furthermore, a <b>new layer for interoperability, interconnectivity and discoverability</b> will be added, facilitating sustainable connections with other (international) research tools. This enables researchers to export and process datasets into their own software and increases the visibility of ODIS in search results of popular search engines. Finally, there will be a <b>new module for collecting and contextualising historical statistical source material</b>. Researchers will be able to download and reuse numerical series and upload their own statistical datasets. 
 <br><br>The programme will not only provide new opportunities for innovative, data-driven research in the human sciences, but will also encourage more research units to join the ODIS partnership and insert their own datasets. By doing so, the thematic scope and research potential of the database will be reinforced. 
 </p>
     </div>
     <div class="shadow-lg p-3 mb-2 my-5 background-bar rounded"></div>
   </div>  
   <div class="px-4p py-2 text-center">
-    <h1 class="display-6 fw-bold text-body-emphasis">Wider renewal</h1>
+    <h1 class="display-6 my-4 fw-bold text-body-emphasis">Wider renewal</h1>
     <div class="col-lg-7 mx-auto my-5">
       <p class="lead mb-4">ODIS' renewal project is not limited to technical improvements, but also focuses on four other aspects:</p>
     </div>
@@ -33,8 +33,8 @@
         <h1 class="my-5">1.	Mission and objectives</h1>
         <p class="lead">We review ODIS' mission and objectives by gathering input from all stakeholders and collaborating with external experts.</p>
       </div>
-      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectMissie.png" alt="" width="345">
+      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectMissie.png" alt="" width="345">
       </div>
     </div>  
     <div class="spacer"></div>
@@ -43,8 +43,8 @@
         <h1 class="my-5">2.	Participation and volunteering</h1>
         <p class="lead">We engage in dialogue with various target groups and potential partners and expand ODIS into a ‘citizen science’ tool. We aim for a collaborative approach by expanding our volunteer group and by using local historical expertise. We do this in cooperation with non-profit organisation (vzw) Histories, culture heritage institutions, etc.</p>
       </div>
-      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectParticipatie.png" alt="" width="345">
+      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectParticipatie.png" alt="" width="345">
       </div>
     </div>
     <div class="spacer"></div>
@@ -53,8 +53,8 @@
         <h1 class="my-5">3.	Input and quality assurance</h1>
         <p class="lead">We are improving the quality and comprehensiveness of existing datasets and adding new data to broaden the thematic scope of ODIS. We are also updating the ‘help’ pages and organising training sessions.</p>
       </div>
-      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectInvoer.png" alt="" width="345">
+      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectInvoer.png" alt="" width="345">
       </div>
     </div>        
     <div class="spacer"></div>
@@ -63,15 +63,15 @@
         <h1 class="my-5">4.	Communication</h1>
         <p class="lead">We are renewing ODIS' corporate identity to give the database a new and modern look and increase name recognition. Furthermore, we are setting up new communication channels, such as social media. This way, we introduce ODIS to potential importer groups and to the wider community even further.</p>
       </div>
-      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectCommunicatie.png" alt="" width="345">
+      <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectCommunicatie.png" alt="" width="345">
       </div>
     </div>
   </div>
-  <div class="shadow-lg p-3 mb-2 my-5 background-bar rounded"></div>
+  <div class="shadow-lg p-3 mb-5 my-5 background-bar rounded"></div>
 
   <div class="px-4 py-6 my-5 text-center">
-    <h1 class="display-5 my-5 fw-bold text-body-emphasis">Team renewal project</h1>
+    <h1 class="display-5 my-5 pt-4 fw-bold text-body-emphasis">Team renewal project</h1>
   </div>
 
 <div class="container col-xxl-8 px-1 py-2">
@@ -79,7 +79,7 @@
     <div class="accordion-item">
       <h2 class="accordion-header">
         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Promotors
+          <b>Promoters</b>
         </button>
       </h2>
       <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionEn">
@@ -91,7 +91,7 @@
     <div class="accordion-item">
       <h2 class="accordion-header">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Team KADOC
+          <b>Team KADOC</b>
         </button>
       </h2>
       <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionEn">
@@ -103,19 +103,19 @@
     <div class="accordion-item">
       <h2 class="accordion-header">
         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Team LIBIS
+          <b>Team LIBIS</b>
         </button>
       </h2>
       <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionEn">
         <div class="accordion-body">
-          Michiel De Clerck, Mehmet Celik, Stephan Pauls
+          Michiel De Clerck, Mehmet Celik, Stefan Pauls
         </div>
       </div>
     </div>
   </div>
 </div>
 
-<div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+<!-- <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
     <div class="px-4 py-6 text-center">
       <a href="https://www.fwo.be/en/"  target="_blank"><img src="https://andras.handl.hu/wp-content/uploads/2018/12/FWO_Logo_Baseline_Lijn_Kleur-1024x150.jpg" alt="FWO" width="200" height="auto"></a>
   </div>      
@@ -132,6 +132,48 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
         <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
     </svg><a href="https://www.odis.be/hercules/_en_home.php"  target="_blank">&nbsp;odis.be</a>
+  </p>
+  </div>
+</div> -->
+
+<div class="d-md-flex d-none flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+    <div class="px-4 py-6 text-center">
+      <a href="https://www.fwo.be/en/"  target="_blank"><img src="https://andras.handl.hu/wp-content/uploads/2018/12/FWO_Logo_Baseline_Lijn_Kleur-1024x150.jpg" alt="FWO" width="200" height="auto"></a>
+  </div>      
+  <div class="text-body-secondary">&copy; 2023 ODIS</div>
+  <div>
+  <p>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+      <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+      <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+    </svg><a class="text-body-secondary" href="tel:016323500">016 32 35 00</a>&nbsp;&nbsp;
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+      <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+    </svg> <a class="text-body-secondary" href = "mailto: info@odis.be">info@odis.be</a> &nbsp;&nbsp;
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
+    </svg>&nbsp;<a class="text-body-secondary" href="https://www.odis.be/hercules/_en_home.php"  target="_blank">odis.be</a>
+  </p>
+  </div>
+</div>
+<div class="d-md-none d-flex flex-column flex-sm-row justify-content-center pt-4 my-4 border-top">
+    <div class="px-4 py-6 text-center">
+      <a href="https://www.fwo.be/en/"  target="_blank"><img src="https://andras.handl.hu/wp-content/uploads/2018/12/FWO_Logo_Baseline_Lijn_Kleur-1024x150.jpg" alt="FWO" width="200" height="auto"></a>
+  </div>      
+  <div class="text-body-secondary pt-2 text-center">&copy; 2023 ODIS</div></div>
+  <div class="d-md-none d-flex flex-column flex-sm-row justify-content-center pb-4 my-4">
+  <div>
+  <p class="text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+      <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+      <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+    </svg><a class="text-body-secondary" href="tel:016323500">016 32 35 00</a>&nbsp;&nbsp;
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+      <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+    </svg> <a class="text-body-secondary" href = "mailto: info@odis.be">info@odis.be</a> &nbsp;&nbsp;<div class="text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
+    </svg>&nbsp;<a class="text-body-secondary" href="https://www.odis.be/hercules/_en_home.php"  target="_blank">odis.be</a></div>
   </p>
   </div>
 </div>
--- src/components/AppTrajectNlODIS.vue
+++ src/components/AppTrajectNlODIS.vue
@@ -2,7 +2,7 @@
     <div class="px-4 py-6 my-5 text-center">
     <h1 class="display-5 my-5 fw-bold text-body-emphasis">ODIS - Vernieuwingstraject 2022-2027</h1>
     <div class="col-lg-7 mx-auto">
-      <p class="lead mb-4">Het Fonds Wetenschappelijk Onderzoek - Vlaanderen (FWO) kende in 2022 een beurs toe voor het project ‘KADOC-ODIS: 'Digitale Onderzoeksinfrastructuur voor Religie, Cultuur en Samenleving', dat voorziet in een grondige vernieuwing van deze KU Leuven Kernfaciliteit. Het investeringsprogramma, dat uitgevoerd wordt in de periode 2022-2027, wil inspelen op de noden van de verschillende ODIS-partners en ervoor zorgen dat de functionaliteiten van ODIS, de databank voor historisch onderzoek naar de samenleving, blijven voldoen aan de vereisten van lopend ​​en toekomstig onderzoek in het veld van de <i>digital humanities</i>.</p>
+      <p class="lead mb-4">Het Fonds Wetenschappelijk Onderzoek - Vlaanderen (FWO) kende in 2022 een beurs toe voor het project 'KADOC-ODIS: Digitale Onderzoeksinfrastructuur voor Religie, Cultuur en Samenleving', dat voorziet in een grondige vernieuwing van deze KU Leuven Kernfaciliteit. Het investeringsprogramma, dat uitgevoerd wordt in de periode 2022-2027, wil inspelen op de noden van de verschillende ODIS-partners en ervoor zorgen dat de functionaliteiten van ODIS, de databank voor historisch onderzoek naar de samenleving, blijven voldoen aan de vereisten van lopend ​​en toekomstig onderzoek in het veld van de <i>digital humanities</i>.</p>
       <div class="d-grid pt-2 gap-2 d-sm-flex justify-content-sm-center">
 <!--        <button type="button" class="btn btn-primary btn-lg px-4 gap-3"><a href="https://www.odis.be/hercules/_nl_home.php" target="_blank">ODIS</a></button>
         <button type="button" class="btn btn-secundary btn-lg px-4"><a href="https://kadoc.kuleuven.be/3_onderzoek/31_onsonderzoek/odis" target="_blank">KADOC-ODIS</a></button>
@@ -36,7 +36,7 @@
         <p class="lead">We herzien de missie en doelstellingen van ODIS door input te verzamelen van alle belanghebbenden en samen te werken met externe experts.</p>
       </div>
       <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
-          <img class="rounded-lg-3" src="/vernieuwing/VernieuwingstrajectMissie.png" alt="" width="340">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectMissie.png" alt="" width="340">
       </div>
     </div>  
     <div class="spacer"></div>
@@ -46,7 +46,7 @@
         <p class="lead">We gaan in dialoog met diverse doelgroepen en potentiële partners en bouwen ODIS uit tot een instrument voor ‘citizen science’. We streven naar een participatieve aanpak door de vrijwilligersgroep uit te breiden en maken gebruik van lokale historische expertise. Dat doen we in samenwerking met Histories vzw, erfgoedcellen, enzovoort.</p>
       </div>
       <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectParticipatie.png" alt="" width="340">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectParticipatie.png" alt="" width="340">
       </div>
     </div>
     <div class="spacer"></div>
@@ -56,7 +56,7 @@
         <p class="lead">We verbeteren de kwaliteit en diepgang van de bestaande datasets en voegen nieuwe gegevens toe om de thematische reikwijdte van ODIS te verbreden. Daarnaast actualiseren we de helppagina’s en organiseren we vormingssessies.</p>
       </div>
       <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectInvoer.png" alt="" width="340">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectInvoer.png" alt="" width="340">
       </div>
     </div>        
     <div class="spacer"></div>
@@ -66,7 +66,7 @@
         <p class="lead">We vernieuwen de huisstijl van ODIS om de databank een nieuw en fris uiterlijk te geven en de herkenbaarheid te vergroten. Daarnaast zetten we nieuwe communicatiekanalen op, zoals sociale media. Zo maken we ODIS verder bekend bij potentiële invoerdersgroepen en in de bredere samenleving.</p>
       </div>
       <div class="col-lg-3 offset-lg-1 p-0 overflow-hidden shadow-lg align-self-end">
-          <img class="rounded-lg-3" mx-auto src="/vernieuwing/VernieuwingstrajectCommunicatie.png" alt="" width="340">
+          <img class="rounded-lg-3 mx-auto d-block" src="/vernieuwing/VernieuwingstrajectCommunicatie.png" alt="" width="340">
       </div>
     </div>
   </div>
@@ -121,7 +121,7 @@
 
 
 
-  <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+  <div class="d-md-flex d-none flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
     <div class="px-4 py-6 text-center">
       <a href="https://www.fwo.be"  target="_blank"><img src="https://andras.handl.hu/wp-content/uploads/2018/12/FWO_Logo_Baseline_Lijn_Kleur-1024x150.jpg" alt="FWO" width="200" height="auto"></a>
   </div>      
@@ -138,6 +138,27 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
         <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
     </svg>&nbsp;<a class="text-body-secondary" href="http://www.odis.be"  target="_blank">odis.be</a>
+  </p>
+  </div>
+</div>
+<div class="d-md-none d-flex flex-column flex-sm-row justify-content-center pt-4 my-4 border-top">
+    <div class="px-4 py-6 text-center">
+      <a href="https://www.fwo.be"  target="_blank"><img src="https://andras.handl.hu/wp-content/uploads/2018/12/FWO_Logo_Baseline_Lijn_Kleur-1024x150.jpg" alt="FWO" width="200" height="auto"></a>
+  </div>      
+  <div class="text-body-secondary pt-2 text-center">&copy; 2023 ODIS</div></div>
+  <div class="d-md-none d-flex flex-column flex-sm-row justify-content-center pb-4 my-4">
+  <div>
+  <p class="text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+      <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+      <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+    </svg><a class="text-body-secondary" href="tel:016323500">016 32 35 00</a>&nbsp;&nbsp;
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+      <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+    </svg> <a class="text-body-secondary" href = "mailto: info@odis.be">info@odis.be</a> &nbsp;&nbsp;<div class="text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe" viewBox="0 0 16 16">
+        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"/>
+    </svg>&nbsp;<a class="text-body-secondary" href="http://www.odis.be"  target="_blank">odis.be</a></div>
   </p>
   </div>
 </div>
